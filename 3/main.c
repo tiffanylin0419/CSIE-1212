ccfe529c4f5b50434c44bf9e51479528b4a79019
林庭瑄 <lintingxuan@lintingxuande-MacBook-Pro.local>
2022-03-19 14:50:32 +0800

    <some message>

--- 3/main.c
+++ 3/main.c
@@ -5,175 +5,167 @@
 #include <memory.h> // memset
 #include <ctype.h>//isdigit
 
-#define myMalloc(x)  (x *)malloc(sizeof(x)) // Macro definition
-
-
-/*----------------- type defined ------------*/
-
 typedef struct node{ 
     int data;
     int group;
     struct node *left, *right; 
-} Node;
+} node;
 
 typedef struct stack{ 
     struct node *top; 
     bool open;
-} Stack;
+} stack;
 
-
-
-/*----------------  function prototype ------------*/
-void cow();
-Stack *alloc_stack();
-Node *find(Stack *, int );
-void push(Stack *,int ,int );
-void pop(Stack *);
-void go(Stack *);
-void print(Stack *S);
-
-
-/*------------- main() --------------*/
-
-int main(){
-    cow();
-    return 0;
+node *alloc(){
+    node *tmp = (node *)malloc(sizeof(node)); 
+    return tmp;
 }
 
-Stack *alloc_stack(){
-    Stack *tmp = myMalloc(Stack); 
-    tmp->top =NULL; 
+void destroy(node *N){ 
+    // clean sensitive data.
+    memset(N, 0, sizeof(*N));
+    free(N); 
+    N=NULL;
+    return;
+}
+
+stack *alloc_stack(){
+    stack *tmp = (stack *)malloc(sizeof(stack)); 
+    tmp->top = NULL;
     tmp->open=true;
     return tmp;
 }
 
-int peep(Stack *S){ // not define prototype yet
-    return S->top->data;
-}
-
-Node *find(Stack *S, int group){
-    Node *tmp=S->top;
-    if (!tmp) { // to prevent shut down when access tmp->group
-		printf("some memory error at S->top\n"); 
-		exit(1);
-	}
-    do {
-        if(tmp->group==group&&tmp->right->group!=group)
+node *find(stack *S,int group){
+    node *tmp=S->top->right;
+    while(tmp!=S->top){
+        if(tmp->group==group&&tmp->right->group!=group){
             return tmp;
+        }
         tmp=tmp->right;
-    }while(tmp!=S->top);
-    
-    return tmp->left;
+    }
+    return S->top;
 }
-
-void push(Stack *S,int data,int group){
-    Node *tmp= myMalloc(Node);
+void push(stack *S,int data,int group){
+    node *tmp=alloc();
     tmp->data=data;
     tmp->group=group;
-    if(S->top==NULL){ 
+    if(S->top==NULL){
         tmp->left=tmp;
         tmp->right=tmp;
         S->top=tmp;
     }
     else{
-        Node *position=find(S,group);
+        node *position=find(S,group);
         tmp->left=position;
         tmp->right=position->right;
         position->right=tmp;
         tmp->right->left=tmp;
+        if(position==S->top){
+            S->top=tmp;
+        }
+
     }
 }
-
-void pop(Stack *S){
-    Node*tmp=S->top;
-    if(tmp && tmp==tmp->right) // only one node
-	    S->top=NULL;
-	else{
-        tmp=tmp->left;
-		tmp->left->right=tmp->right;
-        tmp->right->left=tmp->left;
-        tmp->left=tmp->right=NULL;
+void pop(stack *S){
+    node*tmp=S->top;
+    if(S->top->right==S->top){
+        S->top=NULL;
     }
- //   free(tmp);
+    else{
+        S->top->left->right=S->top->right;
+        S->top->right->left=S->top->left;
+        S->top=S->top->left;
+    }
+    destroy(tmp);
     return;
 }
 
-void go(Stack *S){
-   Node *tmp=S->top;
-    if(tmp && tmp==tmp->right) // only one node
-	    S->top=NULL;
-    else{  	
-        S->top= S->top->right;
-        tmp->left->right=tmp->right;
-        tmp->right->left=tmp->left;
-        tmp->left= tmp->right=NULL;
+void go(stack *S){
+    node *tmp=S->top->right;
+    if(S->top->right==S->top){
+        S->top=NULL;
     }
-    free(tmp);
-    return;    
+    else{
+        S->top->right=S->top->right->right;
+        S->top->right->left=S->top;
+    }
+    destroy(tmp);
+    return;
 }
 
+void destroy_stack(stack *S){
+    while(S->top->right!=NULL){
+        pop(S);
+    }
+    // clean sensitive data.
+    memset(S, 0, sizeof(*S));
+    free(S); 
+    return;
+}
 
-void print(Stack *S){
-    Node* tmp=S->top;
-    
-    if(tmp) {   // not empty
-   		do {
-    		printf("%d ",tmp->data);
+void print(stack *S){
+    node* tmp=S->top;
+
+    if(tmp!=NULL){
+        tmp=tmp->right;
+        while(tmp!=S->top){
+            //printf("%p %d %d %p\n",tmp->left,tmp->data,tmp->group,tmp->right);
+            printf("%d ",tmp->data);
             tmp=tmp->right;
-		} while (tmp!=S->top);
-
-	}
+        }
+        //printf("%p %d %d %p\n",tmp->left,tmp->data,tmp->group,tmp->right);
+        printf("%d",tmp->data);
+    }
     printf("\n");
     return;
 }
 
 
 void cow(){
-    int M=0,N=0,K=0, i;
- //   Stack *lineAry[1000000];  /*--------  TOO BIG to run -----------*/
-     Stack *lineAry[10000]; 
-    scanf("%d %d %d",&M,&N,&K);   
-  //  printf("my test: %d %d %d",M,N,K);  //test
- 
-    for(i=0;i<M;i++){
-        lineAry[i]=alloc_stack();
+    int M=0,N=0,K=0;
+    scanf("%d %d %d",&M,&N,&K);
+
+    stack *S[100000];
+    for(int i=0;i<M;i++){
+        S[i]=alloc_stack();
     }
 
-    for(i=0;i<N;i++){  
-        int group, id, line;
+    for(int i=0;i<N;i++){  
+        int ii,jj,kk;
         char str[10];
-        scanf("%s",str);     
-     
-// if else should change to be switch(str[0]) { case 'e': ¡K }
+        scanf("%s",str);
         if(str[0]=='e'){//enter
-            scanf("%d %d %d",&group,&id,&line);   
-            push(lineAry[line], id, group);
+            scanf("%d %d %d",&ii,&jj,&kk);
+            push(S[kk],jj,ii);
         }
         else if(str[0]=='l'){//leave
-            scanf("%d",&group);  
-            pop(lineAry[group]);
+            scanf("%d",&ii);
+            pop(S[ii]);
         }
         else if(str[0]=='g'){//go
-            scanf("%d",&group);  
-            go(lineAry[group]);
+            scanf("%d",&ii);
+            go(S[ii]);
         }
         else if(str[0]=='c'){//close
-            scanf("%d",&group);  
-            //go(lineAry[group]);
+            scanf("%d",&ii);
+            //go(S[ii]);
         }
 
     }
-
-    for( i=0;i<M;i++)  
-	    print(lineAry[i]);
-    
+    for(int i=0;i<M;i++){
+        print(S[i]);
+    }
     /*
     for(int i=0;i<M;i++){
-        destroy_stack(lineAry[i]);
+        destroy_stack(S[i]);
     }
-    *///
-    
+    */
+    return; 
 }
 
 
-
+int main(){
+    cow();
+    return 0;
+}
