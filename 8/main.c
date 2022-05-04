6186a9f964ccbc8c41bea14183a696cebb2335c7
林庭瑄 <lintingxuan@lintingxuande-MacBook-Pro.local>
2022-05-04 16:14:36 +0800

    <some message>

--- 8/main.c
+++ 8/main.c
@@ -4,29 +4,48 @@
 # include <stdbool.h>
 
 void Magic_Certificate(){
-    char* str = malloc(10000000*sizeof(char));
+    //char* str = malloc(10000000*sizeof(char));
+    char str[10000];
     scanf("%s",str);
     int length=strlen(str);
     int temp_length=length;
-    for(int temp_length=length;temp_length>=0;temp_length--){
-        bool run=true;
+    for(int temp_length=length,temp_start=0;temp_length>=0;temp_length--,temp_start++){
+        //front
+        bool run_front=true;
         for(int i=0,j=temp_length-1;i<=j;i++,j--){
             if(str[i]!=str[j]){
-                run=false;
+                run_front=false;
                 break;
             }
         }
-        if(run){
-            printf("%d\n",length-temp_length);
-            for(int i=length;i>=temp_length;i--){
-                printf("%c",str[i]);
+        //end
+        bool run_end=true;
+        for(int i=temp_start,j=length-1;i<=j;i++,j--){
+            if(str[i]!=str[j]){
+                run_end=false;
+                break;
             }
-            printf("%s\n",str);
+        }
+        //print
+        if(run_front || run_end){
+            printf("%d\n",length-temp_length);
+            if(run_front){
+                for(int i=length;i>=temp_length;i--){
+                    printf("%c",str[i]);
+                }
+                printf("%s\n",str);
+            }
+            if(run_end && length!=temp_length){
+                printf("%s",str);
+                for(int i=temp_start-1;i>=0;i--){
+                    printf("%c",str[i]);
+                }
+                printf("\n");
+            }
             break;
         }
-    }
-    
-    free(str);
+    } 
+    //free(str);
     return;
 }
 
