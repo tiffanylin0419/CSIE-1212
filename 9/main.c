9ad2023edde2da750d36fa78b2ff332722f5bf64
林庭瑄 <lintingxuan@lintingxuande-MacBook-Pro.local>
2022-05-13 15:51:00 +0800

    <some message>

--- 9/main.c
+++ 9/main.c
@@ -59,18 +59,7 @@ void hash(long long* RKP,char** strs,long long k,long long l,long long things,lo
     }
 }
 
-bool similar(char** strs,long long a,long long b,long long l){
-    int different=0;
-    for(int m=1;m<l;m++){
-        if(strs[a][m]!=strs[b][m]){
-            different++;
-        }
-    }
-    if(different<=1){
-        return true;
-    }
-    return false;
-}
+
 void Magic2(){
     long long k,l,flag;
     scanf("%lld %lld %lld",&k,&l,&flag);
@@ -88,13 +77,96 @@ void Magic2(){
     
     //存成一行k個值，sort，任2相等就檢查是不是是答案
     long long* sorted_position= malloc(k*sizeof(long long));
-    //長度=1
-    if(k==1){
+    long long* num= malloc(l*sizeof(long long));
+    long long* RKP_short= malloc(k*sizeof(long long));
+
+    if(flag==0){
+        //長度=1
+        if(k==1){
+            printf("No\n");
+            return;
+        }
+        if(l==1){
+            printf("Yes\n");
+            printf("0 1\n");
+            return;
+        }   
+        
+        //O(l)
+        num[l-1]=1;
+        for(long long i=1;i<l;i++){
+            num[l-1-i]=num[l-i]*things%q;
+        }
+        
+        //O(lklgk)
+        for(long long i=0;i<l;i++){
+            for(long long j=0;j<k;j++){
+                RKP_short[j]=(RKP[j]+q-num[i]*((long long)strs[j][i]-33))%q;
+                sorted_position[j]=j;
+            }
+            //sort
+            merge_sort(RKP_short,sorted_position,0,k-1);
+            long long kk;
+            for(long long j=0;j<k-1;j++){
+                kk=j+1;
+                while(RKP_short[j]==RKP_short[kk] && kk<k){
+                    printf("Yes\n");
+                    printf("%lld %lld\n",sorted_position[j],sorted_position[kk]);
+                    return;
+                    if(kk>=k-1){
+                        break;
+                    }
+                    kk++;
+                }
+            }
+            
+        }
         printf("No\n");
-        return;
     }
-    if(l==1){
-        /*
+    else{
+        //長度=1
+        if(k==1){
+            printf("No\n");
+            printf("0\n");
+            return;
+        }
+        if(l==1){
+            printf("Yes\n");
+            printf("%lld\n",k*(k-1)/2);
+            return;
+        }
+        
+        int count=0;
+        //O(l)
+        num[l-1]=1;
+        for(long long i=1;i<l;i++){
+            num[l-1-i]=num[l-i]*things%q;
+        }
+            
+        //O(lklgk)
+        for(long long i=0;i<l;i++){
+            for(long long j=0;j<k;j++){
+                RKP_short[j]=(RKP[j]+q-num[i]*((long long)strs[j][i]-33))%q;
+                sorted_position[j]=j;
+            }
+            //sort
+            merge_sort(RKP_short,sorted_position,0,k-1);
+            long long kk;
+            for(long long j=0;j<k-1;j++){
+                kk=j+1;
+                while(RKP_short[j]==RKP_short[kk] && kk<k){
+                    count++;
+                    if(kk>=k-1){
+                        break;
+                    }
+                    kk++;
+                }
+            }
+            
+        }
+        printf("%d\n",count);
+        //算全部的
+        
         for(long long j=0;j<k;j++){
             sorted_position[j]=j;
         }
@@ -104,49 +176,7 @@ void Magic2(){
             kk=j+1;
             //printf("%lld %lld,%lld %lld\n",j,kk,RKP_short[j],RKP_short[kk]);
             while(RKP[j]==RKP[kk] && kk<k){
-                printf("Yes\n");
-                printf("%lld %lld\n",sorted_position[j],sorted_position[kk]);
-                return;
-                kk++;
-            }
-        }
-        */
-        printf("Yes\n");
-        printf("0 1\n");
-        return;
-    }   
-    
-    //O(l)
-    long long* num= malloc(l*sizeof(long long));
-    num[l-1]=1;
-    for(long long i=1;i<l;i++){
-        num[l-1-i]=num[l-i]*things%q;
-    }
-    
-    //O(lklgk)//?
-    long long* RKP_short= malloc(k*sizeof(long long));
-    for(long long i=0;i<l;i++){
-        for(long long j=0;j<k;j++){
-            RKP_short[j]=(RKP[j]+q-num[i]*((long long)strs[j][i]-33))%q;
-            sorted_position[j]=j;
-        }
-        //sort
-        merge_sort(RKP_short,sorted_position,0,k-1);
-        long long kk;
-        for(long long j=0;j<k-1;j++){
-            kk=j+1;
-            while(RKP_short[j]==RKP_short[kk] && kk<k){
-                //?
-                printf("Yes\n");
-                printf("%lld %lld\n",sorted_position[j],sorted_position[kk]);
-                return;
-                /*
-                if(similar(strs,sorted_position[j],sorted_position[kk],l)){
-                    printf("Yes\n");
-                    printf("%lld %lld\n",sorted_position[j],sorted_position[kk]);
-                    return;
-                }
-                */
+                count-=l-1;
                 if(kk>=k-1){
                     break;
                 }
@@ -154,9 +184,18 @@ void Magic2(){
             }
         }
         
-    }
-    printf("No\n");
     
+        if(count==0){
+            printf("No\n");
+            printf("0\n");
+        }  
+        else{
+            printf("Yes\n");
+            printf("%d\n",count);
+        }
+
+    }
+
     for(long long i=0;i<k;i++){
         free(strs[i]);
     }
@@ -165,7 +204,6 @@ void Magic2(){
     free(num);
     free(RKP_short);
     free(sorted_position);
-    
 }
 
 int main(){
