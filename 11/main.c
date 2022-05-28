8bd94dc494bc6b67fae8c658aa6dcc14b674cd98
b07501113 <b07501113@g.ntu.edu.tw>
2022-05-28 21:37:22 +0800

    <some message>

--- 11/main.c
+++ 11/main.c
@@ -4,12 +4,14 @@
 #include <string.h>
 
 
-void PurpleCow(int start,int M,int N){
+void CoolServer(int start,int M,int N){
     
 }
-int arr[100000]={0};
-long long sum[100000]={0};
 
+int arr[100001]={0};
+long long sum[100001]={0};
+int command[100000][5]={0};
+int add[100000]={0};
 int main() {
     // save input info in command
     
@@ -22,14 +24,48 @@ int main() {
         sum[i]=sum[i-1]+arr[i];
     }
 
-    int action;
-    int l;
-    int r;
     for(int i=0;i<Q;i++){
-        scanf("%d",&action);
-        if(action==6){
-            scanf("%d %d",&l,&r);
-            printf("%lld\n",sum[r]-sum[l-1]);
+        scanf("%d",&command[i][0]);
+        if(command[i][0]==1){
+            scanf("%d %d",&command[i][1],&command[i][2]);
+        }else if(command[i][0]==2){
+            scanf("%d",&command[i][1]);
+        }else if(command[i][0]==3){
+            scanf("%d %d",&command[i][1],&command[i][2]);
+        }else if(command[i][0]==4){
+            scanf("%d %d %d %d",&command[i][1],&command[i][2],&command[i][3],&command[i][4]);
+        }else if(command[i][0]==5){
+            scanf("%d %d %d",&command[i][1],&command[i][2],&command[i][3]);
+        }else{
+            scanf("%d %d",&command[i][1],&command[i][2]);
+        }
+    }
+    int add_len=0;
+    for(int i=0;i<Q;i++){
+        if(command[i][0]==1){
+            N++;
+            add[add_len]=i;
+            add_len++;
+        }else if(command[i][0]==2){
+            break;
+        }else if(command[i][0]==3){
+            break;
+        }else if(command[i][0]==4){
+            break;
+        }else if(command[i][0]==5){
+            break;
+        }else if(command[i][0]==6){
+            int add_num=0;
+            for(int j=0;j<add_len;j++){
+                if(command[i][1]<=command[add[j]][1]+1 && command[add[j]][1]+1<=command[i][2]){
+                    command[i][2]--;
+                    add_num+=command[add[j]][2];
+                }else if(command[add[j]][1]+1<command[i][1]){
+                    command[i][1]--;
+                    command[i][2]--;
+                }
+            }
+            printf("%lld\n",sum[command[i][2]]-sum[command[i][1]-1]+add_num);
         }
     }
 }
